4f8fe3ef60cef3860e8bbe99003dc3a0433b483d
DohWon <estote@gmail.com>
2021-12-20 22:28:57 +0900

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Kim Byeong-ock on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
